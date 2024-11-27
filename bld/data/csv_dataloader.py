f4c5a4093e9c8e2dccf71267d93b5b3ddeb9bfad
szuzina <108017846+szuzina@users.noreply.github.com>
2024-11-27 22:09:22 +0100

    Update csv_dataloader.py
    upload without json

--- bld/data/csv_dataloader.py
+++ bld/data/csv_dataloader.py
@@ -12,35 +12,29 @@ class CSVDataLoader:
 
         self.csv_data_id = csv_data_id
         self.root_folder = root_folder
-        self.data = self.create_json()
-        self.patient_data = self.data[self.data['source_index'] == p_number]
+        
+        self.data = self.upload_csv_dir()
+
+        self.patient_data = self.find_patient_data()
         self.filtered_scores = self.find_filtered_scores(filtered_rows=idx)
 
-    def create_json(self):
+
+    def upload_csv_dir(self):
         drive_url = 'https://drive.google.com/uc?export=download&id='
         csv_directory_url = drive_url + self.csv_data_id
         # download the csv directory
         gdown.download(csv_directory_url, output=self.root_folder+'/bld/data/csv_zip', quiet=False)
         with zipfile.ZipFile(self.root_folder + '/bld/data/csv_zip', 'r') as zip_ref:
             zip_ref.extractall(self.root_folder + '/bld/data/csv_dir')
+        return 0
 
-        # create json file
+    def find_patient_data(self):
+
         csv_directory = self.root_folder + '/bld/data/csv_dir'
-        json_file_path = self.root_folder + '/bld/data/combined_data.json'
+        patient_path = csv_directory + f'/patient{self.p_number}.csv'
+        df = pd.read_csv(patient_path, header=None, sep=';')
 
-        dataframes = []
-
-        for idx, csv_file in enumerate(glob.glob(os.path.join(csv_directory, '*.csv')), start=1):
-            df = pd.read_csv(csv_file, header=None, sep=';')
-            df['source_index'] = idx  # Add source index column
-            dataframes.append(df)
-
-        combined_df = pd.concat(dataframes, ignore_index=True)
-        combined_df.to_json(json_file_path, orient='records', lines=True)
-
-        print(f"Combined JSON file saved as {json_file_path}")
-
-        return combined_df
+        return df
 
     def find_filtered_scores(self, filtered_rows):
         filtered_scores = self.patient_data.loc[self.patient_data.iloc[:, 0].isin(filtered_rows)].iloc[:, 1].tolist()
