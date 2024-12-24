4c605f395cfc51ceada3e3fd9989a4e7fc60c5dd
szuzina <108017846+szuzina@users.noreply.github.com>
2024-12-25 00:21:09 +0100

    Update csv_dataloader.py
    dir upload moved to DataDownloader

--- bld/data/csv_dataloader.py
+++ bld/data/csv_dataloader.py
@@ -8,9 +8,8 @@ import pandas as pd
 
 class CSVDataLoader:
 
-    def __init__(self, p_number, idx, csv_data_id, root_folder='/content'):
+    def __init__(self, p_number, idx, root_folder='/content'):
 
-        self.csv_data_id = csv_data_id
         self.root_folder = root_folder
         self.p_number = p_number
         
@@ -19,15 +18,6 @@ class CSVDataLoader:
         self.patient_data = self.find_patient_data()
         self.filtered_scores = self.find_filtered_scores(filtered_rows=idx)
 
-
-    def upload_csv_dir(self):
-        drive_url = 'https://drive.google.com/uc?export=download&id='
-        csv_directory_url = drive_url + self.csv_data_id
-        # download the csv directory
-        gdown.download(csv_directory_url, output=self.root_folder+'/bld/data/csv_zip', quiet=False)
-        with zipfile.ZipFile(self.root_folder + '/bld/data/csv_zip', 'r') as zip_ref:
-            zip_ref.extractall(self.root_folder + '/bld/data/csv_dir')
-        return 0
 
     def find_patient_data(self):
 
